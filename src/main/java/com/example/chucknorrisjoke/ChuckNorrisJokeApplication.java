e53444226470515a99188b1652abc0d723239f88
vpeitton <vpeitton@gmail.com>
2021-06-09 16:46:49 -0300

    commit 1

---
+++ src/main/java/com/example/chucknorrisjoke/ChuckNorrisJokeApplication.java
@@ -0,0 +1,13 @@
+package com.example.chucknorrisjoke;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChuckNorrisJokeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ChuckNorrisJokeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/chucknorrisjoke/ChuckNorrisJokeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.chucknorrisjoke;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChuckNorrisJokeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
